6369a832edb5cfb7605a6851b5382268414f2d53
Kris Wawrzyniak <kwawrzyniak@ufl.edu>
2019-02-27 15:26:14 -0500

    Updated schemas

--- server/resources/events/schema.js
+++ server/resources/events/schema.js
@@ -1,17 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 var eventSchema = new Schema({
-    //TODO: IMPLEMENT SCHEMA FOR EVENTS
-
-
+    //TODO: ADD ANY SCHEMA INFO I MISSED
+    name: String,
+    date: Date,
+    info: String
 });
-
-
-
-
-
 
 var Events = mongoose.model('members', eventSchema,'events');
 module.exports = Events;
