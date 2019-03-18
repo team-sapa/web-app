1fa4f56321c70a6e7bbbfb1211519479a5a60981
NotAnAndroid <banouge@gmail.com>
2019-03-18 17:12:53 -0400

    added server event controller (needs implementation)

---
+++ server/resources/events/eventController.js
@@ -0,0 +1,6 @@
+//get mongoose and event schema
+var mongoose = require('mongoose');
+var Event = require('./schema.js');
+
+//TO DO: put functions here (look at /server/controllers/listings.server.controller.js on bootcamp 4)
+//exports.foo = function(req, res) {}
