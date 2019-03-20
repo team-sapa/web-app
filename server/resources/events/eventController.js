48ab0025e2aace963a1bc6980a2f9e1ed63976f3
NotAnAndroid <banouge@gmail.com>
2019-03-19 21:00:10 -0400

    added empty functions to server event controller

--- server/resources/events/eventController.js
+++ server/resources/events/eventController.js
@@ -4,3 +4,26 @@ var Event = require('./schema.js');
 
 //TO DO: put functions here (look at /server/controllers/listings.server.controller.js on bootcamp 4)
 //exports.foo = function(req, res) {}
+
+//creates event and saves it to database
+//input is event (req.body)
+exports.create = function (req, res) {
+    //TO DO: fill this in
+}
+
+//deletes event from database
+//input is event (req.event)
+exports.destroy = function (req, res) {
+    //TO DO: fill this in
+}
+
+//updates event in database
+//input is old event (req.event) and new event (req.body)
+exports.update = function (req, res) {
+    //TO DO: fill this in
+}
+
+//get array containing all events
+exports.getAll = function (req, res) {
+    //TO DO: fill this in
+}
