14bd46834c7e6000e1acc65c6bf9fa364a876d6a
NotAnAndroid <banouge@gmail.com>
2019-03-26 20:55:23 -0400

    implemented exports.create in events.js

--- server/resources/events/events.js
+++ server/resources/events/events.js
@@ -10,8 +10,23 @@ var mongoose = require('mongoose'),
 
     //CREATE NEW EVENT (ADMIN)
     exports.create = (req, res) => {
-        console.log("test2");
-        res.json("nice2");
+    //    console.log("test2");
+    //    res.json("nice2");
+
+        //create event
+        var event = new Event(req.body);
+
+        //save event
+        event.save(function (err) {
+            if (err) {
+                //print and send error
+                console.log(err);
+                res.status(400).send(err);
+            } else {
+                //return event
+                res.json(event);
+            }
+        });
     };
 
     //DISPLAYS SINGLE EVENT'S INFO
