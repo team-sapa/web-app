ec4c65ccd1b22b7862114fb18b84f92f63444c31
NotAnAndroid <banouge@gmail.com>
2019-04-14 11:57:59 -0400

    events sort by date, date is easier to read

--- client/controllers/eventController.js
+++ client/controllers/eventController.js
@@ -3,11 +3,20 @@ angular.module('sapaApp').controller('eventController', ['$scope', 'Main',
 
         // check & set access level/auth token
         $scope.accessLevel = 1;
-
+     
         Main.listEvents().then(function (response) {
             $scope.events = response.data;
+
             $scope.events.forEach(function (event) {
-                event.date2 = event.date.substring(0, 10);
+                event.date2 = new Date(event.date);
+            })
+
+            $scope.events.sort(function compareEvents(a, b) {
+                return a.date2.getTime() - b.date2.getTime();
+            });
+
+            $scope.events.forEach(function (event) {
+                event.date2 = event.date2.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
             })
         }, function (error) {
             console.log('Unable to retrieve events:', error);
